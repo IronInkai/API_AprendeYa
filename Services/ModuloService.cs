6903db203cc99c27de474aa3009086b7869ed9b2
IronInkai <i202315741@cibertec.edu.pe>
2026-04-29 01:05:42 -0500

    Se implemento el metodo GetModulosByCurso

--- Services/ModuloService.cs
+++ Services/ModuloService.cs
@@ -17,12 +17,26 @@ namespace API_AprendeYa.Services
             return db.Query<Modulo>("SELECT * FROM modulo").ToList();
         }
 
+        public List<Modulo> GetModulosByCurso(int idCurso)
+        {
+            using var db = new SqlConnection(_connection);            
+            string sql = @"SELECT id_modulo AS IdModulo, 
+                          id_curso AS IdCurso, 
+                          titulo AS Titulo, 
+                          descripcion AS Descripcion, 
+                          orden AS Orden 
+                   FROM modulo 
+                   WHERE id_curso = @id";
+            return db.Query<Modulo>(sql, new { id = idCurso }).ToList();
+        }
+
         public Modulo GetModuloById(int idModulo)
         {
             using var db = new SqlConnection(_connection);
-            return db.QueryFirstOrDefault<Modulo>(
-                "SELECT * FROM modulo WHERE id_modulo = @id",
-                new { id = idModulo });
+            string sql = @"SELECT id_modulo AS IdModulo, id_curso AS IdCurso, 
+                          titulo AS Titulo, descripcion AS Descripcion, orden AS Orden 
+                   FROM modulo WHERE id_modulo = @id";
+            return db.QueryFirstOrDefault<Modulo>(sql, new { id = idModulo });
         }
 
         public bool InsertModulo(Modulo modulo)
